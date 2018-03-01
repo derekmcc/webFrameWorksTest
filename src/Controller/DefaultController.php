22963fb910d9d8070278620872173152bae1c864
derekmcc <derek5mccarthy@gmail.com>
2018-03-01 21:38:02 +0000

    Added default controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        return new Response("Home Page");
+    }
+}
