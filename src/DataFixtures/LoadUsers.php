398b88d3ed2f6431a7b119152fd284de57c029ea
derekmcc <derek5mccarthy@gmail.com>
2018-03-17 22:50:08 +0000

    Added User class for logins and user data fixtures

---
+++ src/DataFixtures/LoadUsers.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 17/03/2018
+ * Time: 22:21
+ */
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class LoadUsers extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // create objects
+        $userUser = $this->createUser('user', 'user');
+        $userAdmin = $this->createUser('admin', 'admin', ['ROLE_ADMIN']);
+        $userMatt = $this->createUser('derek', 'mccarthy', ['ROLE_ADMIN']);
+
+        // store to DB
+        $manager->persist($userUser);
+        $manager->persist($userAdmin);
+        $manager->persist($userMatt);
+        $manager->flush();
+    }
+
+    /**
+     * @param $username
+     * @param $plainPassword
+     * @param array $roles // default to ROLE_USER if no ROLE supplied
+     *
+     * @return User
+     */
+    private function createUser($username, $plainPassword, $roles = ['ROLE_USER']):User
+    {
+        $user = new User();
+        $user->setUsername($username);
+        $user->setRoles($roles);
+
+        // password - and encoding
+        $encodedPassword = $this->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
+
+        return $user;
+    }
+
+    private function encodePassword($user, $plainPassword):string
+    {
+        $encodedPassword = $this->encoder->encodePassword($user, $plainPassword);
+        return $encodedPassword;
+    }
+}
---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 17/03/2018
+ * Time: 22:48
+ */
+
+namespace App\Security;
+
+
+class AccessDeniedHandler
+{
+
+}
